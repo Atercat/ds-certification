4bd5f983618501d9bbfd3ea50766936be52691e8
Alexey Kotov <atercat@gmail.com>
2022-07-13 10:26:40 +0300

    Added forgotten output

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,9 +3,6 @@ module "vm" {
     name = "builder"
     key_pair = "wsl-work"
 }
-#resource "null_resource" "playbook" {
-#  provisioner "local-exec" {
-#    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu -e 'vm_ip=${module.vm.ip}' main.yaml"
-#  }
-#  depends_on = [module.vm]
-#}
+output "ip" {
+    value = module.vm.ip
+}
